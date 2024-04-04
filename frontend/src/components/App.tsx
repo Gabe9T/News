102f267a5e02fa598859b5d0f566bcfb28d07d8c
gabe <gabrieltucker260@gmail.com>
2024-04-03 22:50:30 -0700

    moved app to comps

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Frontpage from './Frontpage';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Frontpage />
+    </div>
+  );
+}
+
+export default App;
